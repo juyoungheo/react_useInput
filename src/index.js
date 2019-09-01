7fb3eb05866d1fe908e0a464412fc98a073f73a0
juyoungheo <heojjuyoungg@gmail.com>
2019-09-01 21:34:00 +0900

    react
    useInput

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 function App() {
+  const maxLen = value => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
